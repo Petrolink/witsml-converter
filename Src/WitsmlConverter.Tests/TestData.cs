13ad36b6acc808b4a532b8a4926890f3c87b9f57
Joel Lang <85507227+JoelDavidLang@users.noreply.github.com>
2023-05-11 17:14:28 -0500

    Add WITSML 1.4 source data for tests

---
+++ Src/WitsmlConverter.Tests/TestData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolink.WitsmlConverter.Tests;
+
+internal static class TestData
+{
+    public static string Get14SourcePath(string type)
+    {
+        return Path.Combine(AppContext.BaseDirectory, "TestData", "Witsml14Source", $"{type}_no_xsl.xml");
+    }
+    public static string Get14As20Path(string type)
+    {
+        return Path.Combine(AppContext.BaseDirectory, "TestData", "Witsml14As20", $"{type}.xml");
+    }
+
+    public static string Read14SourceData(string type)
+    {
+        return File.ReadAllText(Get14SourcePath(type));
+    }
+
+    public static readonly IReadOnlyList<(string, string?)> Witsml14To20Types = new[] {
+        ("attachment", null),
+        ("bharun", null),
+        ("cementjob", null),
+        ("drillReport", null),
+        ("fluidsreport", null),
+        ("formationMarker", "wellboremarker"),
+        ("log", null),
+        ("mudlog", "mudlogreport"),
+        ("opsreport", null),
+        ("rig", null),
+        ("rig", "rigUtilization"),
+        ("risk", null),
+        ("stimjob", null),
+        ("surveyprogram", null),
+        ("toolErrorModel", null),
+        ("toolErrorTermSet", null),
+        ("trajectory", null),
+        ("tubular", null),
+        ("wbgeometry", "wellboregeometry"),
+        ("well", null),
+        ("wellbore", null),
+        ("mudlog", "wellboregeology"),
+    };
+}
---
+++ Src/WitsmlConverter.Tests/TestHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Petrolink.WitsmlConverter.Tests;
+
+internal static class TestHelper
+{
+    internal static bool EqualsXml(string a, string b)
+    {
+        // load and compare the values of two XML documents and ignore creation time
+        throw new NotImplementedException();
+    }
+}
