93558eb05f0892a0368774ced8a3ded4834a6f2f
Joel Lang <85507227+JoelDavidLang@users.noreply.github.com>
2023-05-11 17:12:26 -0500

    Fix to test project build, and some simple tests

---
+++ Src/WitsmlConverter.Tests/Witsml20UnitConverterTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Petrolink.WitsmlConverter.Tests;
+
+public class Witsml20UnitConverterTests
+{
+    [Theory]
+    [InlineData(true, "CV", "hp[metric]")]
+    [InlineData(true, "MY", "Ma[t]")]
+    [InlineData(true, "1000ft3", "1000 ft3")]
+    [InlineData(false, "unknown/unit", null)]
+    public void TryConverter14To20_14Units_20Units(bool success, string before, string? after)
+    {
+        var result = Witsml20UnitConverter.TryConvert14To20(before, out var resultUnit);
+
+        Assert.Equal(success, result);
+        Assert.Equal(after, resultUnit);
+    }
+
+    [Theory]
+    [InlineData(true, "hp[metric]", "CV")]
+    [InlineData(true, "Ma[t]", "MY")]
+    [InlineData(true, "1000 ft3", "1000ft3")]
+    [InlineData(false, "unknown/unit", null)]
+    public void TryConverter20To14_20Units_14Units(bool success, string before, string? after)
+    {
+        var result = Witsml20UnitConverter.TryConvert20To14(before, out var resultUnit);
+
+        Assert.Equal(success, result);
+        Assert.Equal(after, resultUnit);
+    }
+}
