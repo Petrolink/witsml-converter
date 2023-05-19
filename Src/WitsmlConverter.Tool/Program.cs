01b52a776cfa7914c4d9c6ef9da81ad4bcae76b3
Joel Lang <85507227+JoelDavidLang@users.noreply.github.com>
2023-05-19 07:55:51 -0500

    Support patterns with directory inputs

--- Src/WitsmlConverter.Tool/Program.cs
+++ Src/WitsmlConverter.Tool/Program.cs
@@ -1,12 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.CommandLine;
+using Microsoft.Extensions.FileSystemGlobbing;
 using Petrolink.WitsmlConverter;
 
-var inputOption = new Option<List<string>>(new[] { "--input", "-i" }, "Input file paths for the transformation") { IsRequired = true };
-var outputOption = new Option<string>(new[] { "--output", "-o" }, "Output directory") { IsRequired = true };
-var typeOption = new Option<string>(new[] { "--type", "-t" }, "The output object type") { IsRequired = true };
-var modeOption = new Option<WitsmlTransformType>(new[] { "--transform", "-x" }, "The transformation type") { IsRequired = true };
-var overwriteOption = new Option<bool>(new[] { "--overwrite", "-f" }, "Whether to overwrite destination files");
+const string DefaultPattern = "*.xml";
+
+var inputOption = new Option<IList<string>>(new[] { "--input", "-i" }, "An input file or directory path for the transformation. May be specified multiple times. Directory contents will be matched using the --patterns option.") { IsRequired = true };
+var outputOption = new Option<string>(new[] { "--output", "-o" }, "The output directory.") { IsRequired = true };
+var typeOption = new Option<string>(new[] { "--type", "-t" }, "The output object type.") { IsRequired = true };
+var modeOption = new Option<WitsmlTransformType>(new[] { "--transform", "-x" }, "The transformation type.") { IsRequired = true };
+var patternsOption = new Option<IList<string>>(new[] { "--patterns", "-p" }, () => new[] { DefaultPattern }, "An input file pattern used when matching against input directories. May be specified multiple times.");
+var overwriteOption = new Option<bool>(new[] { "--overwrite", "-f" }, "Whether to overwrite destination files.");
 
 var rootCommand = new RootCommand("WITSML Converter");
 
@@ -16,21 +20,43 @@ var transformCommand = new Command("transform", "Transform one or more WITSML do
     outputOption,
     typeOption,
     modeOption,
+    patternsOption,
     overwriteOption
 };
 
 rootCommand.AddCommand(transformCommand);
 
-transformCommand.SetHandler(ExecuteTransform, inputOption, outputOption, typeOption, modeOption, overwriteOption);
+transformCommand.SetHandler(ExecuteTransform,
+                            inputOption,
+                            outputOption,
+                            typeOption,
+                            modeOption,
+                            patternsOption,
+                            overwriteOption);
 
 return rootCommand.Invoke(args);
 
 // Execute the transform command
-void ExecuteTransform(List<string> input, string output, string type, WitsmlTransformType conversion, bool overwrite)
+void ExecuteTransform(
+    IList<string> input,
+    string output,
+    string type,
+    WitsmlTransformType conversion,
+    IList<string> patterns,
+    bool overwrite)
 {
     // TODO Detect destination type based on the input
+    var matcher = new Matcher();
 
-    List<string> inputFilePaths = input.SelectMany(ResolveFiles).ToList();
+    matcher.AddIncludePatterns(patterns);
+
+    var inputFilePaths = input.SelectMany(p => ResolveFiles(p, matcher)).ToList();
+
+    if (inputFilePaths.Count == 0)
+    {
+        WriteError("No valid inputs found");
+        return;
+    }
 
     PathType outputType = GetPathType(output);
 
@@ -69,13 +95,13 @@ void ExecuteTransform(List<string> input, string output, string type, WitsmlTran
 }
 
 // Resolves an input file or folder to a list of files
-IEnumerable<string> ResolveFiles(string input)
+IEnumerable<string> ResolveFiles(string input, Matcher matcher)
 {
     return GetPathType(input) switch
     {
         PathType.None => throw DefaultException($"Path does not refer to a file or directory: {input}"),
         PathType.File => new[] { Path.GetFullPath(input) },
-        PathType.Directory => Directory.EnumerateFiles(input),
+        PathType.Directory => matcher.GetResultsInFullPath(input),
         _ => throw new NotImplementedException()
     };
 }
